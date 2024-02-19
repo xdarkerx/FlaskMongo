7ebed68c7beb4f48200c70f48693622075c0716a
Vinicius Nunes <vmendesnunes1@gmail.com>
2024-02-18 22:54:10 -0300

    opa

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+if __name__ == '__main__':
+    app.run()
 
 @app.route('/')
 def home():
@@ -9,3 +16,7 @@ def home():
 @app.route('/dashboard/')
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
